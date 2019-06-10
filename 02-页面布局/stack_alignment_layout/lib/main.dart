eb42e9c091f2a09db0a166893bf8a8e3da032bdf
lining <helloworld@126.com>
2019-06-10 08:37:12 +0800

    页面布局

---
+++ 02-页面布局/stack_alignment_layout/lib/main.dart
@@ -0,0 +1,89 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: '用Stack实现层叠布局',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: '用Stack实现层叠布局'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      // This call to setState tells the Flutter framework that something has
+      // changed in this State, which causes it to rerun the build method below
+      // so that the display can reflect the updated values. If we changed
+      // _counter without calling setState(), then the build method would not be
+      // called again, and so nothing would appear to happen.
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // 用Stack实现层叠布局
+
+    var stack = new Stack(
+      alignment: Alignment.bottomRight,
+      children: <Widget>[
+
+        new Text("hello",style:new TextStyle(
+          fontSize: 30,
+          color:Colors.blue
+        )),
+        new Text('我是超人，yeah',style:new TextStyle(
+          fontSize:30,
+          color:Colors.red
+        ))
+      ],
+    );
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text('Stack Alignment'),
+      ),
+      body:new Center(
+        child:stack
+      )
+    );
+
+
+  }
+}
